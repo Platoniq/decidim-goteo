5490296a4277d8ccc51080c3f6ba4a26a5d43816
Alejandro Puerta <alejandro.puerta@nazaries.com>
2023-06-15 15:56:20 +0200

    fix: when the organization has sign in and sing up disabled, does not ask for password update

---
+++ spec/lib/overrides_spec.rb
@@ -0,0 +1,35 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+# We make sure that the checksum of the file overriden is the same
+# as the expected. If this test fails, it means that the overriden
+# file should be updated to match any change/bug fix introduced in the core
+checksums = [
+  {
+    package: "decidim-core",
+    files: {
+      "/app/models/decidim/user.rb" => "944e4d222c903008e2dc2262a0ac0d73"
+    }
+  }
+]
+
+describe "Overriden files", type: :view do
+  # rubocop:disable Rails/DynamicFindBy
+  checksums.each do |item|
+    spec = ::Gem::Specification.find_by_name(item[:package])
+
+    item[:files].each do |file, signature|
+      it "#{spec.gem_dir}#{file} matches checksum" do
+        expect(md5("#{spec.gem_dir}#{file}")).to eq(signature)
+      end
+    end
+  end
+  # rubocop:enable Rails/DynamicFindBy
+
+  private
+
+  def md5(file)
+    Digest::MD5.hexdigest(File.read(file))
+  end
+end
